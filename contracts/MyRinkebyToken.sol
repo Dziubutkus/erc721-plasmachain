60bb0b0324d0cb70e47edaa64be879158a811229
Dziugas Butkus <dziubutkus@gmail.com>
2019-04-20 23:27:00 -0600

    added skills for the token for the future

--- contracts/MyRinkebyToken.sol
+++ contracts/MyRinkebyToken.sol
@@ -3,16 +3,64 @@ pragma solidity ^0.5.2;
 import "openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol";
 
 contract MyRinkebyToken is ERC721Full {
+
+    struct Hero {
+        uint strength;
+        uint defense;
+        uint agility;
+    }
+
+    Hero[] heroes;
+
     constructor(string memory name, string memory symbol) public ERC721Full(name, symbol) {
 
     }
 
-    function mint(uint256 _tokenId) public
-    {
-        _mint(msg.sender, _tokenId);
-    }
-
     function depositToGateway(address _gateway, uint256 _tokenId) public {
         safeTransferFrom(msg.sender, _gateway, _tokenId);
+    }
+
+    function getHero(uint _tokenId) returns(
+        uint strength,
+        uint defense,
+        uint agility
+    ){
+        Hero memory hero = heroes[_tokenId];
+
+        strength = hero.strength;
+        defense = hero.defense;
+        agility = hero.agility;
+    }
+
+    function mint(
+        uint _strength,
+        uint _defense,
+        uint _agility
+    )
+    public
+    {
+        _mintHero(_strength, _defense, _agility, msg.sender);
+    }
+
+    function _mintHero(
+        uint _strength,
+        uint _defense,
+        uint _agility,
+        address _owner
+    )
+        internal
+        returns (uint)
+    {
+        Hero memory _hero = Hero({
+            strength: _strength,
+            defense: _defense,
+            agility: _agility,
+        });
+
+        uint256 newHeroId = heroes.push(_hero) - 1;
+
+        _mint(_owner, newHeroId);
+
+        return newHeroId;
     }
 }
